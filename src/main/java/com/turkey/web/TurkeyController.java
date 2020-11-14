36556b101071a707d8eabace57de9054dcd3aed8
Derek Lee <dlee133@gmail.com>
2020-11-14 10:26:50 -0500

    init

--- src/main/java/com/turkey/web/TurkeyController.java
+++ src/main/java/com/turkey/web/TurkeyController.java
@@ -1,57 +1,72 @@
 package com.turkey.web;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 import com.turkey.business.Turkey;
-@RestController
-@RequestMapping("/turkey")
-public class TurkeyController {
-	private List<Turkey> turkeys = new ArrayList<>();
-	
+import com.turkey.db.TurkeyRepo;
 
+
+@CrossOrigin
+@RestController
+@RequestMapping("/turkeys")
+public class TurkeyController {
+	
+/*
+ *  A controller will implement 5 CRUD methods
+ *  1.) get all
+ *  2.) get by id
+ *  3.) post - insert
+ *  4.) put - Update
+ *  5.) DELETE - Delete
+ */
+	@Autowired
+	private TurkeyRepo turkeyRepo;
+	
+	// get all Turkeys
 	@GetMapping("/")
-	// get all (select *- no filter
-	public List<Turkey>  getTurkeys() {
-		return turkeys;
+	public List<Turkey> getAll() {
+		return turkeyRepo.findAll();
 	}
+	// Get a turkey by id
 	@GetMapping("/{id}")
-	// get a single turkey by id
-	public Turkey getTurkey(@PathVariable int id) {
-		Turkey t = null;
-		for (Turkey turkey : turkeys) {
-			if (turkey.getId() == id) {
-				t = turkey;
-			}
-		}
-		return t;
+	public Optional<Turkey> getById(@PathVariable int id) {
+		return turkeyRepo.findById(id);
+	
+	
+	
 	}
-	// when we accept request parameters we don't need a leading forward slash "/"
-	@PostMapping("")
-	public Turkey createTurkey( @RequestParam int id, @RequestParam String name,@RequestParam double weight) {
-		Turkey t = new Turkey(id,name, weight);
-		turkeys.add(t);
-		return t;
+	// Add a turkey
+	@PostMapping("/")
+	public Turkey addTurkey(@RequestBody Turkey m) {
+	m =	turkeyRepo.save(m);
+	return m;
 	}
-	// delete a turkey by id (delete where id=?)
+	
+	// update turkey
+	@PutMapping("/")
+	public Turkey updateTurkey(@RequestBody Turkey m) {
+		m = turkeyRepo.save(m);
+		return m;
+	}
+	// update turkey
 	@DeleteMapping("/{id}")
 	public Turkey deleteTurkey(@PathVariable int id) {
-		Turkey t = null;
-		for (Turkey turkey: turkeys) {
-			if (turkey.getId() == id) {
-				t = turkey;
-				turkeys.remove(t);
-			}
-		}
-		return t;
-	}
+		Optional<Turkey> m = turkeyRepo.findById(id);
+		if (m.isPresent()) {
 
+		turkeyRepo.deleteById(id);
+		}
+		else {
+			System.out.println("Error turkey not found for id" + id);
+		}
+		return m.get();
+	}
+	
+	
+	
 }
